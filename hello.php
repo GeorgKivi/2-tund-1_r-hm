e3e3e1dd853fb67fb570a9787b4c4a3289d9d801
Georg Kivi <georkiv@greeny.cs.tlu.ee>
2015-09-07 14:59:07 +0300

    create user

--- hello.php
+++ hello.php
@@ -1,9 +1,43 @@
 <?php
 
-// annan väärtuse sõnale "georg"
-$first_name ="georg";
-$last_name = "kivi";
+		// annan väärtuse sõnale "georg"
+		$first_name ="georg";
+		$last_name = "kivi";
 
-//trükib välja
-echo $first_name." ".$last_name;
+		//trükib välja
+		echo $first_name." ".$last_name;
+
+?>
+
+<br>
+
+<?php
+
+	$age = 20;
+	
+	// if(loogikatehe){tõene} else{väär}
+	if($age < 18){ 
+		echo "sa oled alaealine";
+		
+	}else{
+		echo "oled täisealine";
+	}
+
+?>
+<br>
+<?php
+
+	for($i = 0; $i < $age; $i = $i +1){
+		//se mida korratakse
+		echo"palju, ";
+		
+	}
+	echo "õnne";
+?>
+<br>
+
+<?php
+//kuupäev
+echo date("l, j F Y e");
+
 ?>
---
+++ user_form.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+<html>
+	<head>
+		<title>User login page</title>
+	</head>
+	<body>
+		<h2>login</h2>
+		<input type="email" placeholder="E-post"><br><br>
+		<input type="password" placeholder="parool">
+		
+		<input type ="submit" value="logi sisse">
+		<h2>Create user</h2>
+	</body>
+</html>
